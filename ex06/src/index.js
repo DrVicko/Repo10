40c767dc23b15207e33c9a7445c8112e93d802b9
Vedran V <49645956+DrVicko@users.noreply.github.com>
2022-04-06 19:35:03 +0200

    ex06

---
+++ ex06/src/index.js
@@ -0,0 +1,24 @@
+// Only change code below this line
+
+var lion = {
+    name: "Simba",
+    legs: 4,
+    tails: 1
+};
+// End of  object
+
+// function below
+function myFunction(propName, propValue) {
+    
+    lion[propName] = propValue; 
+
+    return lion;
+}
+
+
+myFunction("roar", "roar-roar");
+
+// Only change code above this line
+
+
+module.exports = { school, myFunction }; 
