218e97cfc4f2db64d566a5918fbdd16f46f052bc
Vedran V <49645956+DrVicko@users.noreply.github.com>
2022-04-06 19:52:50 +0200

    ex08

---
+++ ex08/src/index.js
@@ -0,0 +1,21 @@
+function myObject() {
+    'use strict';
+    const MATH_CONSTANTS = {
+        E: 2.71828
+    };
+    
+    //bla bla
+    Object.freeze(MATH_CONSTANTS);
+    //bla bla
+
+    try {
+        MATH_CONSTANTS.E = 23;
+    } catch (error) {
+        console.log(error.message);
+    }
+    return MATH_CONSTANTS.E; 
+}
+
+const E = myObject();
+
+module.exports = myObject;
